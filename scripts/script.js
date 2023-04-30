8facea10bd830643e9e575e7ebcc1c38313cf103
Jess Shi <jess.shi@mail.utoronto.ca>
2023-04-30 01:32:36 -0400

    Beautifully polished nav!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
+
+const displayPageLocation = () => {
+
+    const options = {
+        threshold: 0.5
+    }
+    
+    const observer = new IntersectionObserver((entries) => {
+    
+        entries.forEach(entry => {
+    
+            const sectionLink = document.querySelector(`a[href="#${entry.target.id}"]`);
+    
+            if (entry.isIntersecting) {
+                sectionLink.classList.add("displayed");
+    
+            } else {
+                sectionLink.classList.remove("displayed");
+            }
+        })
+    }, options);
+    
+    const sections = document.querySelectorAll("#home, #about, #skills, #projects, #contact");
+    
+    sections.forEach(section => {
+    
+        observer.observe(section);
+    });
+}
+
+
+displayPageLocation();
+
 // document.querySelector("button").addEventListener("click", () => {
 //     document.documentElement.style.setProperty('--accent-colour', '#00ff00');
 // })
