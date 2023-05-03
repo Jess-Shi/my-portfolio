5a49504fbda5f1a791039168c274b8b0a38d2de6
Jess Shi <jess.shi@mail.utoronto.ca>
2023-05-03 01:02:39 -0400

    Set up theme toggle logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,41 @@
 
+
+const setUpThemeToggle = () => {
+
+    const lightTheme = {
+        "--accent-colour": "#002266",
+        "--body-background": "#ffffff",
+        "--primary-font-colour": "#000000",
+    }
+
+    const darkTheme = {
+        "--accent-colour": "#ffb400",
+        "--body-background": "#000000",
+        "--primary-font-colour": "#ffffff",
+    }
+
+    const themeButton = document.querySelector(".theme-toggle");
+
+    themeButton.addEventListener("click", () => {
+
+        if (themeButton.classList.contains("dark-theme")) {
+            for (let key in lightTheme) {
+                document.documentElement.style.setProperty(key, lightTheme[key]);
+            }
+            themeButton.innerHTML = `<i class="fa-solid fa-moon" title="Click to toggle dark theme">`;
+
+        } else {
+            for (let key in darkTheme) {
+                document.documentElement.style.setProperty(key, darkTheme[key]);
+            }
+            themeButton.innerHTML = `<i class="fa-solid fa-sun" title="Click to toggle light theme">`;
+        }
+
+        themeButton.classList.toggle("dark-theme");
+        themeButton.classList.toggle("light-theme");
+    });
+}
+
 const displayPageLocation = () => {
 
     const options = {
@@ -28,9 +65,5 @@ const displayPageLocation = () => {
     });
 }
 
-
+setUpThemeToggle();
 displayPageLocation();
-
-// document.querySelector("button").addEventListener("click", () => {
-//     document.documentElement.style.setProperty('--accent-colour', '#00ff00');
-// })
